e818d1c839fd5b534e466e6ca7bccc2db69a0959
denis <denis.c1@live.fr>
2020-12-03 04:35:28 +0100

    gnl

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/22 17:35:37 by user42            #+#    #+#             */
-/*   Updated: 2020/11/23 19:00:49 by user42           ###   ########.fr       */
+/*   Updated: 2020/11/30 08:56:33 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,15 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-int		get_next_line(int fd, char **line);
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strdup(const char *s);
+
+char	*trim_line(char **linebuf);
+int		read_line(int fd, char **linebuf);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strcat_alloc(char const *s1, char const *s2);
 
 #endif
