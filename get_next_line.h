8b1ebcd0b150d0938c334ca74a1b99be8bf49f04
denis <denis.c1@live.fr>
2020-12-03 04:51:34 +0100

    correction .h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/22 17:35:37 by user42            #+#    #+#             */
-/*   Updated: 2020/11/30 08:56:33 by user42           ###   ########.fr       */
+/*   Updated: 2020/12/03 04:51:20 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strdup(const char *s);
 
-char	*trim_line(char **linebuf);
-int		read_line(int fd, char **linebuf);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strcat_alloc(char const *s1, char const *s2);
+int		get_next_line(int fd, char **line);
 
 #endif
